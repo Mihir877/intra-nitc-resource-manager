aa1216637e7050d09dfe1ab2e9b7db69c36b5a6b
Saurabh Tripathi <saurabh2002oct13@gmail.com>
2025-11-01 23:46:51 +0530

    checkpoint

--- client/src/components/ui/textarea.jsx
+++ client/src/components/ui/textarea.jsx
@@ -1,38 +1,19 @@
-<<<<<<< HEAD
 import * as React from "react";
-import { cn } from "@/lib/utils";
-=======
-import * as React from "react"
 
-import { cn } from "@/lib/utils"
->>>>>>> 3c18cfa38f62516aaebc6a500aa6628d8b907878
+import { cn } from "@/lib/utils";
 
 const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <textarea
-<<<<<<< HEAD
-      ref={ref}
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
+      ref={ref}
       {...props}
     />
   );
 });
 Textarea.displayName = "Textarea";
 
-export default Textarea;
-=======
-      className={cn(
-        "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
-      )}
-      ref={ref}
-      {...props} />
-  );
-})
-Textarea.displayName = "Textarea"
-
-export { Textarea }
->>>>>>> 3c18cfa38f62516aaebc6a500aa6628d8b907878
+export { Textarea };
